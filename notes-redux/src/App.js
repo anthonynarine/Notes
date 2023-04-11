b1ad15afe6d651fe5db044e5cdab50dc4778f058
anthonynarine <anarine83@gmail.com>
2023-04-11 17:49:06 -0400

    fetchall request updated with actual URL i find this easier to setup up and read. commented out call from api file

--- notes-redux/src/App.js
+++ notes-redux/src/App.js
@@ -4,26 +4,38 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setNoteList } from "./store/notes-slice";
 import { NoteAPI } from "./api/notes-api";
+import axios from "axios";
 
 function App() {
   const dispatch = useDispatch()
 
+
+  //THIS  SETUP UTILIZED THE API SETUP IN ./api/note-api
+  // useEffect(() => {
+  //   async function fetchAllNotes(){
+  //     try {
+  //       const noteList = await NoteAPI.fetchAll()
+  //       dispatch(setNoteList(noteList))  
+  //       console.log(noteList)  
+  //     } catch (error) {
+  //       console.log(error)    
+  //     };
+  //   }
+  //   fetchAllNotes();
+  // });
+
   useEffect(() => {
     async function fetchAllNotes(){
       try {
-        const noteList = await NoteAPI.fetchAll()
-        dispatch(setNoteList(noteList))  
-        console.log(noteList)  
+        const {data} = await axios.get("http://127.0.0.1:8000/api/notes/")
+        dispatch(setNoteList(data))  
+        console.log("Note List", data)  
       } catch (error) {
         console.log(error)    
       };
     }
     fetchAllNotes();
   });
-
-
-
-
 
 
   return (
