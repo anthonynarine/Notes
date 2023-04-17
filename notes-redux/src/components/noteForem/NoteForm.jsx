d080ffa22d16abb107550e16688f7df5acca699b
anthonynarine <anarine83@gmail.com>
2023-04-17 18:00:01 -0400

    create a note functional. not happy with how this process was buit and mui styling makes code difficult to read but functionality works

--- notes-redux/src/components/noteForem/NoteForm.jsx
+++ notes-redux/src/components/noteForem/NoteForm.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { Container, Grid, TextField, Paper, Typography, Button } from "@mui/material";
-
+import {
+  Container,
+  Grid,
+  TextField,
+  Paper,
+  Typography,
+  Button,
+} from "@mui/material";
 import { Create, Delete } from "@mui/icons-material";
-
+import { useState } from "react";
 
 const pageStyles = {
   btn: {
     backgroundColor: "black",
-    color:"white",
+    color: "white",
     width: "60%",
-    marginBottom:1,
+    marginBottom: 1,
     borderRadius: 3,
     "&:hover": {
       backgroundColor: "#FF3F00",
@@ -17,11 +23,20 @@ const pageStyles = {
   },
 };
 
+function NoteForm({ title, onClickEdit, onClickDelete, onSubmit }) {
+  //state to hold form values
+  const [formValues, setFormValues] = useState({ title: "", body: "" });
 
-function NoteForm({title, onClickEdit, onClickDelete, onSubmit}) {
+  const updateFormValues = (event) => {
+    const id = event.target.id;
+    const value = event.target.value;
+    // spreading the note content is needed here or it will remove all other content if edited
+    setFormValues({...formValues, [id]: value });
+  };
 
   const actionIcons = (
     <>
+      {/* edit and delete icons will only be shown if the event is sent via props. */}
       <div>{onClickEdit && <Create />}</div>
       <div>{onClickDelete && <Delete />}</div>
     </>
@@ -29,65 +44,88 @@ function NoteForm({title, onClickEdit, onClickDelete, onSubmit}) {
 
   const titleInput = (
     <>
-      <TextField color="warning" id="title" label="Title" variant="outlined" />
+      <TextField
+      // onChange to update input values
+        onChange={updateFormValues}
+        color="warning"
+        id="title"
+        label="Title"
+        variant="outlined"
+      />
     </>
   );
   const contentInput = (
     <>
-    <TextField
-      multiline
-      color="warning"
-      rows={4}
-      fullWidth
-      id="title"
-      label="Content"
-      variant="outlined"
-    />
+      <TextField
+        onChange={updateFormValues}
+        multiline
+        color="warning"
+        rows={4}
+        fullWidth
+        id="body"
+        label="Body"
+        variant="outlined"
+      />
     </>
   );
 
-  const submitBtn = <><Button
-  sx={pageStyles.btn}
-  >Submit</Button></>;
+  const submitBtn = (
+    <>
+      <Button onClick={()=>onSubmit(formValues)} sx={pageStyles.btn}>Submit</Button>
+    </>
+  );
 
   return (
-    <Container >
-      <Paper elevation={15} sx={{marginTop:"50px", paddingBottom:"1px"}}> 
-      <Grid container justifyContent="center" marginTop="50px">
-        <Grid
-          container
-          spacing={1}
-          justifyContent="space-between"
-          marginBottom="40px"
-          marginTop="25px"
-          marginLeft="50px"
-          marginRight="50px"
-          xs={12}
-        >
-          <Grid item sx={8}>
-            <Typography variant="h6" >New note</Typography>
-          </Grid>
-          <Grid item sx={4}>
-            {actionIcons}
+    <Container>
+      <Paper elevation={15} sx={{ marginTop: "50px", paddingBottom: "1px" }}>
+        <Grid container justifyContent="center" marginTop="50px">
+          <Grid
+            container
+            spacing={1}
+            justifyContent="space-between"
+            marginBottom="40px"
+            marginTop="25px"
+            marginLeft="50px"
+            marginRight="50px"
+            xs={12}
+          >
+            <Grid item sx={8}>
+              <Typography variant="h6">New note</Typography>
+            </Grid>
+            <Grid item sx={4}>
+              {actionIcons}
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-      <form>
+        <form>
+          <Grid
+            xs={12}
+            item
+            alignItems="start"
+            marginLeft="50px"
+            marginBottom="30px"
+          >
+            {titleInput}
+          </Grid>
+          <Grid
+            xs={12}
+            marginBottom="30px"
+            item
+            marginLeft="50px"
+            marginRight="50px"
+          >
+            {contentInput}
+          </Grid>
+        </form>
         <Grid
           xs={12}
-          item
-          alignItems="start"
-          marginLeft="50px"
           marginBottom="30px"
+          item
+          marginLeft="28%"
+          marginRight="50px"
         >
-          {titleInput}
-        </Grid>
-        <Grid xs={12} marginBottom="30px" item marginLeft="50px" marginRight="50px">
-          {contentInput}
-        </Grid>
-      </form>
-        <Grid xs={12} marginBottom="30px" item marginLeft="28%" marginRight="50px">
-          {submitBtn}
+          {/* only renders if onSubmit is sent via props */}
+          {onSubmit && submitBtn}
         </Grid>
       </Paper>
     </Container>
--- notes-redux/src/pages/NoteCreate.jsx
+++ notes-redux/src/pages/NoteCreate.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import NoteForm from '../components/noteForem/NoteForm'
+import axios from 'axios'
+import { NoteAPI } from '../api/notes-api'
+import { useDispatch } from 'react-redux'
+import { addNote } from '../store/notes-slice'
+import { useNavigate } from 'react-router-dom'
 
 function NoteCreate() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const submit = async (formValues) =>{
+    try {
+      const createNote = await axios.post("http://127.0.0.1:8000/api/notes/create/", formValues);
+      console.log(formValues)
+      dispatch(addNote(createNote))
+      alert("note created")     
+    } catch (error) {
+      console.log(error)     
+    }
+    navigate("/");
+  }
+
+
   return (
     <div>
       
-      <NoteForm title="New Note" />
+      <NoteForm title="New Note" onSubmit={submit} />
 
     </div>
   )
